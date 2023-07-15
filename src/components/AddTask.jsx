073ae2851beaac5b7ae48f2853055f747e57a24d
itayG98 <itai.g98@gmail.com>
2023-07-15 11:38:50 +0300

    add form validation

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,18 +1,82 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const AddTask = function AddTask() {
+const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [isTitleValid, setIsTitleValid] = useState(false);
+  const [isTitleTouched, setIsTitleTouched] = useState(false);
+
+  const [description, setDescription] = useState("");
+  const [isDescriptionTouched, setIsDescriptionTouched] = useState(false);
+  const [isDescriptionValid, setIsDescriptionValid] = useState(false);
+
+  const [formValidity, setFormValidity] = useState(false);
+
+  const isValidLength = (value) => value.trim().length >= 3;
+
+  const onUnFocusedHandler = (event, setter, validitySetter, touchSetter) => {
+    const value = event.target.value;
+    touchSetter(true);
+    setter(value);
+    const isValid = isValidLength(value);
+    validitySetter(isValid);
+    setFormValidity(isTitleValid && isDescriptionValid);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    setTitle("");
+    setIsTitleTouched(false);
+    setIsTitleValid(false);
+    setDescription("");
+    setIsDescriptionTouched(false);
+    setIsDescriptionValid(false);
+    setFormValidity(false);
+    event.target.reset();
+  };
+
   return (
-    <form>
+    <form onSubmit={(event) => onSubmitHandler(event)}>
       <div>
-        <label htmlFor="name">
-          Enter tasks name
-          <input type="text" placeholder="Reading" />
+        <label htmlFor="title">
+          Enter task name
+          <input
+            className={isTitleTouched && !isTitleValid ? "invalid" : ""}
+            type="text"
+            placeholder="Reading"
+            onChange={(event) =>
+              onUnFocusedHandler(
+                event,
+                setTitle,
+                setIsTitleValid,
+                setIsTitleTouched
+              )
+            }
+          />
         </label>
       </div>
       <div>
         <label htmlFor="description">
-          Enter tasks description
-          <input type="text" placeholder="Read 15 pages a day" />
+          Enter task description
+          <input
+            className={
+              isDescriptionTouched && !isDescriptionValid ? "invalid" : ""
+            }
+            type="text"
+            placeholder="Read 15 pages a day"
+            onChange={(event) =>
+              onUnFocusedHandler(
+                event,
+                setDescription,
+                setIsDescriptionValid,
+                setIsDescriptionTouched
+              )
+            }
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="submit">
+          <input type="submit" disabled={!formValidity} />
         </label>
       </div>
     </form>
